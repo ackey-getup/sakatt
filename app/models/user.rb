f0ab76f2d6408cb5aa330a7107cb63e7b0ac429c
ackey-getup <akira.getup@gmail.com>
2021-05-11 20:56:13 +0900

    アソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
 end
